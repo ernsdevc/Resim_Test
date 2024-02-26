4a01184a6febb01e7c1784ab8f56dd15930a2120
Erensu Deveci <erensudeveci@gmail.com>
2024-02-26 18:38:17 +0300

    Add project files.

---
+++ Resim_Test/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resim_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string resim;
+        Color renk;
+        Bitmap bmp;
+
+        private void btnResimSec_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            resim = openFileDialog1.FileName;
+        }
+
+        private void btnRenkSec_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            renk = colorDialog1.Color;
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(resim);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.DrawString(txtMetin.Text, new Font("Seoge UI", Convert.ToInt16(txtBoyut.Text), FontStyle.Bold), new SolidBrush(renk), 20, 30);
+            pictureBox1.Image = bmp;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "resim|.jpg";
+            saveFileDialog1.ShowDialog();
+            bmp.Save(saveFileDialog1.FileName);
+        }
+    }
+}
